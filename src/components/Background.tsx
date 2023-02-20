d088e548c3f9d7d713fce285e864d99342d2b104
Syncxv <aruldevarackal1@gmail.com>
2023-02-21 08:04:02 +1100

    debug texture

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -4,21 +4,23 @@ import { scene, setAnimateCallbacks } from '../three'
 import { camera } from '../three/index'
 export const Background: Component = () => {
 	onMount(() => {
-		//create plane mesh
-		const plane = new THREE.Mesh(
-			new THREE.PlaneGeometry(100, 100),
-			new THREE.MeshBasicMaterial({ color: 0x0000ff, side: THREE.DoubleSide })
-		)
+		//texture material
+		const texture = new THREE.TextureLoader().load('/debug-texture.jpg')
+		const material = new THREE.MeshBasicMaterial({
+			map: texture,
+			side: THREE.DoubleSide
+		})
+		const plane = new THREE.Mesh(new THREE.PlaneGeometry(500, 500), material)
 		plane.rotateX(-Math.PI / 2)
 		plane.position.set(0, 0, -1)
 		scene.add(plane)
 
-		setAnimateCallbacks((prev) => [
-			...prev,
-			() => {
-				console.log(camera.position)
-			}
-		])
+		// setAnimateCallbacks((prev) => [
+		// 	...prev,
+		// 	() => {
+		// 		console.log(camera.rotation)
+		// 	}
+		// ])
 	})
 	return <div>hu</div>
 }
